ae6ea08d1f7e307c5daf03b14ca93cebbd53be6f
yorch4 <jorge.rgdaw@gmail.com>
2019-11-22 14:14:22 +0100

    IMAGEN

---
+++ tartas.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <?php include ('includes/enlaces.php'); ?>
+        <title></title>
+    </head>
+    <style>
+        div {
+            border: solid 1px;
+        }
+    </style>
+    <body>
+        <?php
+        include('includes/header.php');
+        ?>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <img src="">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            TEXTO
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-8">
+                    IMAGEN
+                </div>
+            </div>
+        </div>
+        <?php
+        include('includes/footer.php');
+        ?>
+    </body>
+</html>
