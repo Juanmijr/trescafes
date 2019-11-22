2830d0f488b12e6b61355784f6259d1612ccba6b
lolotr99 <manolotoro80@gmail.com>
2019-11-22 14:56:04 +0100

    Añado productos.php

---
+++ productos.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <?php 
+        include('includes/enlaces.php');
+        ?>
+        <title></title>
+    </head>
+    <body>
+        <?php
+         include('includes/header.php');
+        ?>
+        
+        <div class="row">
+            <div class="col-sm-3">
+                
+            </div>
+            <div class="col-sm-6 text-center pt-5 text-secondary">
+                PRODUCTOS
+            </div>
+            <div class="col-sm-3"></div>
+        </div>
+        
+        <div class="row">
+            <div class="col-sm-4">
+                
+            </div>
+            <div class="col-sm-4 text-center pt-5 text-secondary">
+                CAFÉ
+            </div>
+            <div class="col-sm-4">
+                
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="col-sm-4">
+                <img class="imgRedes " src="img/415.png"></img>
+            </div>
+            <div class="col-sm-4">
+                <img class="imgRedes " src="img/415.png"></img>
+            </div>
+            <div class="col-sm-4">
+                <img class="imgRedes " src="img/415.png"></img>
+            </div>
+        </div>
+
+        
+        <div class="row">
+            <div class="col-sm-4">
+                
+            </div>
+            <div class="col-sm-4 text-center pt-5 text-secondary">
+                REPOSTERÍA
+            </div>
+            <div class="col-sm-4">
+                
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="col-sm-4">
+                <img class="imgRedes " src="img/415.png"></img>
+            </div>
+            <div class="col-sm-4">
+                <img class="imgRedes " src="img/415.png"></img>
+            </div>
+            <div class="col-sm-4">
+                <img class="imgRedes " src="img/415.png"></img>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-4">
+                <img class="imgRedes " src="img/415.png"></img>
+            </div>
+            <div class="col-sm-4">
+                
+            </div>
+            <div class="col-sm-4">
+
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="col-sm-4">
+                
+            </div>
+            <div class="col-sm-4 text-center pt-5 text-secondary">
+                OTROS
+            </div>
+            <div class="col-sm-4">
+                
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="col-sm-4">
+                <img class="imgRedes " src="img/415.png"></img>
+            </div>
+            <div class="col-sm-4">
+                <img class="imgRedes " src="img/415.png"></img>
+            </div>
+            <div class="col-sm-4">
+
+            </div>
+        </div>
+        <?php      
+         include('includes/footer.php');
+        ?>
+    </body>
+</html>
