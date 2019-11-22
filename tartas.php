c386f6f31ca985befec6976801fbf1bd2e3ee739
yorch4 <jorge.rgdaw@gmail.com>
2019-11-22 14:54:54 +0100

    TARTAS

--- tartas.php
+++ tartas.php
@@ -6,34 +6,26 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <?php include ('includes/enlaces.php'); ?>
+        <?php include ('includes/header.php'); ?>
         <title></title>
     </head>
-    <style>
-        div {
-            border: solid 1px;
-        }
-    </style>
     <body>
-        <?php
-        include('includes/header.php');
-        ?>
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-4">
+        <div>
+            <div class="row align-items-center">
+                <div class="col-sm-4 mt-5">
                     <div class="row">
                         <div class="col-sm-12">
-                            <img src="">
+                            <a href="index.php"> <img src="img/volver.png"></a>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-sm-12">
-                            TEXTO
+                    <div class="row mt-5 mx-auto">
+                        <div class="col-sm-12 mt-10 text-secondary text-justify">
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                         </div>
                     </div>
                 </div>
                 <div class="col-sm-8">
-                    IMAGEN
+                    <img class="imgProductos" src="img/Prueba.png">
                 </div>
             </div>
         </div>
