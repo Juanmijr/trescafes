8f09e1fb29ea3cf9e38260733cf53ea94855cbe3
DWES <DWES@10.0.2.15>
2019-11-23 12:08:36 +0100

    Añado empleo.php y quienesSomos.php

---
+++ quienesSomos.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <?php include ('includes/enlaces.php'); ?>
+        <title></title>
+    </head>
+    <body>
+        <?php
+         include ('includes/header.php');
+        ?>
+        
+        <div class="row">
+            <div class="col-sm-8 text-center pt-5 text-justify text-secondary">
+                <p class="text-title">¿QUIÉNES SOMOS?</p>
+                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ultricies varius enim at lacinia. Maecenas non tristique libero, vel semper diam. Praesent in velit nec orci lobortis consectetur sit amet non nisi. Donec dapibus volutpat ex, id consectetur quam egestas quis. Quisque tellus eros, suscipit lobortis pellentesque quis, lacinia eu diam. Donec nibh arcu, feugiat eu rhoncus et, aliquet sed odio. Quisque dapibus auctor nulla vel congue. Ut elementum nibh non dolor tempor pellentesque eget at est. Vivamus a quam sed tellus dapibus molestie in quis nisi. Proin eget vulputate purus. Nam consequat sodales ante. Nunc blandit vestibulum ex ut posuere. Praesent orci felis, auctor eget dui vel, cursus fringilla ligula. Quisque quis tincidunt felis.  </p>
+            </div>
+            <div class="col-sm-4 mt-5">
+                <img class="imgRedes " src="img/415.png"></img>
+            </div>
+        </div>
+        
+        <?php      
+         include('includes/footer.php');
+        ?>
+    </body>
+</html>
