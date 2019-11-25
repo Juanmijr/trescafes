ce0b55cd421186ab9556b44a8b4d1c6c409e87cc
yorch4 <jorge.rgdaw@gmail.com>
2019-11-25 11:11:36 +0100

    TUSDATOS

---
+++ tusdatos.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <?php include ('includes/enlaces.php'); ?>
+        <title></title>
+    </head>
+    <body>
+        <?php include ('includes/header.php'); ?>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="my-5">
+                        <img class="imgTusDatos border" src="img/415.png">
+                    </div>
+                    <section>
+                        <span class='text-title'>TU NOMBRE</span>
+                        <p class="text-secondary text-center">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but als</p>
+                    </section>
+                    <section class="my-5">
+                        <span class='text-title'>CUPONES</span>
+                        <p class="text-secondary text-center">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but als</p>
+                    </section>
+                    <section>
+                        <span class='text-title'>HISTORIAL DE COMPRAS</span>
+                        <p class="text-secondary text-center">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but als</p>
+                    </section>
+                </div>
+            </div>
+        </div>
+        <?php
+        include('includes/footer.php');
+        ?>
+    </body>
+</html>
