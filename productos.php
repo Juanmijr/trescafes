fc2f3d72af75a876fe2e251357ee23b89ba3667e
lolotr99 <manolotoro80@gmail.com>
2019-11-25 13:23:07 +0100

    Añado mi parte de los ficheros

--- productos.php
+++ productos.php
@@ -17,20 +17,10 @@ and open the template in the editor.
         ?>
         
         <div class="row">
-            <div class="col-sm-3">
-                
-            </div>
-            <div class="col-sm-6 text-center pt-5 text-secondary">
-                PRODUCTOS
-            </div>
-            <div class="col-sm-3"></div>
-        </div>
-        
-        <div class="row">
             <div class="col-sm-4">
                 
             </div>
-            <div class="col-sm-4 text-center pt-5 text-secondary">
+            <div class="col-sm-4 text-center pt-5 text-title">
                 CAFÉ
             </div>
             <div class="col-sm-4">
@@ -40,13 +30,19 @@ and open the template in the editor.
         
         <div class="row">
             <div class="col-sm-4">
-                <img class="imgRedes " src="img/415.png"></img>
+                <img class="imgRedes " src="img/415.png"/>
+                <span class="text-secondary">Loloccino</span>
+                <a href="loloccino.php"> <img src="img/siguiente.png"></a>
             </div>
             <div class="col-sm-4">
-                <img class="imgRedes " src="img/415.png"></img>
+                <img class="imgRedes " src="img/415.png"/>
+                <span class="text-secondary">Yorspresso</span>
+                <a href="yorspresso.php"> <img src="img/siguiente.png"></a>
             </div>
             <div class="col-sm-4">
-                <img class="imgRedes " src="img/415.png"></img>
+                <img class="imgRedes " src="img/415.png"/>
+                <span class="text-secondary">Juanmocha</span>
+                <a href="juanmocha.php"> <img src="img/siguiente.png"></a>
             </div>
         </div>
 
@@ -55,7 +51,7 @@ and open the template in the editor.
             <div class="col-sm-4">
                 
             </div>
-            <div class="col-sm-4 text-center pt-5 text-secondary">
+            <div class="col-sm-4 text-center pt-5 text-title">
                 REPOSTERÍA
             </div>
             <div class="col-sm-4">
@@ -65,18 +61,26 @@ and open the template in the editor.
         
         <div class="row">
             <div class="col-sm-4">
-                <img class="imgRedes " src="img/415.png"></img>
+                <img class="imgRedes " src="img/415.png"/>
+                <span class="text-secondary">Tartas</span>
+                <a href="tartas.php"> <img src="img/siguiente.png"></a>
             </div>
             <div class="col-sm-4">
-                <img class="imgRedes " src="img/415.png"></img>
+                <img class="imgRedes " src="img/415.png"/>
+                <span class="text-secondary">Magdalenas</span>
+                <a href="magdalenas.php"> <img src="img/siguiente.png"></a>
             </div>
             <div class="col-sm-4">
-                <img class="imgRedes " src="img/415.png"></img>
+                <img class="imgRedes " src="img/415.png"/>
+                <span class="text-secondary">Galletas</span>
+                <a href="galletas.php"> <img src="img/siguiente.png"></a>
             </div>
         </div>
         <div class="row">
             <div class="col-sm-4">
-                <img class="imgRedes " src="img/415.png"></img>
+                <img class="imgRedes " src="img/415.png"/>
+                <span class="text-secondary">Bizcochos</span>
+                <a href="bizcochos.php"> <img src="img/siguiente.png"></a>
             </div>
             <div class="col-sm-4">
                 
@@ -90,7 +94,7 @@ and open the template in the editor.
             <div class="col-sm-4">
                 
             </div>
-            <div class="col-sm-4 text-center pt-5 text-secondary">
+            <div class="col-sm-4 text-center pt-5 text-title">
                 OTROS
             </div>
             <div class="col-sm-4">
@@ -100,10 +104,14 @@ and open the template in the editor.
         
         <div class="row">
             <div class="col-sm-4">
-                <img class="imgRedes " src="img/415.png"></img>
+                <img class="imgRedes " src="img/415.png"/>                
+                <span class="text-secondary">Chocolate</span>
+                <a href="chocolates.php"> <img src="img/siguiente.png"></a>
             </div>
             <div class="col-sm-4">
-                <img class="imgRedes " src="img/415.png"></img>
+                <img class="imgRedes " src="img/415.png"/>
+                <span class="text-secondary">Infusiones</span>
+                <a href="infusiones.php"> <img src="img/siguiente.png"></a>
             </div>
             <div class="col-sm-4">
 
