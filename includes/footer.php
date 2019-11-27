0989bfca4a30fb7614d50d940de38ebbaea2b1b9
yorch4 <jorge.rgdaw@gmail.com>
2019-11-27 12:14:15 +0100

    CONTAINER

--- includes/footer.php
+++ includes/footer.php
@@ -3,124 +3,125 @@
         <?php include ('includes/enlaces.php')?>
     </head>
     <body>
-         <!-- Footer -->
-        <footer class="page-footer lighten-3 pt-4  bg-primary text-footer mt-5">
+            <!-- Footer -->
+           <footer class="page-footer lighten-3 pt-4  bg-primary text-footer mt-5">
 
-            <!-- Footer Links -->
-            <div class="container text-center">
+               <!-- Footer Links -->
+               <div class="container text-center">
 
-                <!-- Grid row -->
-                <div class="row">
+                   <!-- Grid row -->
+                   <div class="row">
 
-                    <!-- Grid column -->
-                    <div class="col-sm-3">
+                       <!-- Grid column -->
+                       <div class="col-sm-3">
 
-                        <!-- Content -->
-                        <h5 class="font-weight-bold text-uppercase mb-4">TrésCaf€s</h5>
-                        <p class="text-footer-cuerpo">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit
-                            amet numquam iure provident
-                            voluptate
-                            esse
-                            quasi, veritatis totam voluptas nostrum.</p>
+                           <!-- Content -->
+                           <h5 class="font-weight-bold text-uppercase mb-4">TrésCaf€s</h5>
+                           <p class="text-footer-cuerpo">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit
+                               amet numquam iure provident
+                               voluptate
+                               esse
+                               quasi, veritatis totam voluptas nostrum.</p>
 
-                    </div>
-                    <!-- Grid column -->
-
-                    
-                    <!-- Grid column -->
-                    <div class="col-sm-3">
-
-                        <!-- Links -->
-                        <h5 class="font-weight-bold text-uppercase mb-4">About</h5>
-
-                        <ul class="list-unstyled text-footer-cuerpo ">
-                            <li>
-                                <p>
-                                    <a class="footer-letra" href="calidad.php">CALIDAD</a>
-                                </p>
-                            </li>
-                            <li>
-                                <p>
-                                    <a class="footer-letra" href="productos.php">PRODUCTOS</a>
-                                </p>
-                            </li>
-                            <li>
-                                <p>
-                                    <a class="footer-letra" href="empleo.php">EMPLEO</a>
-                                </p>
-                            </li>
-                            <li>
-                                <p>
-                                    <a class="footer-letra" href="">¡AL GRANO!</a>
-                                </p>
-                            </li>
-                        </ul>
-
-                    </div>
-                    <!-- Grid column -->
-
-                    <!-- Grid column -->
-                    <div class="col-sm-3">
-
-                        <!-- Contact details -->
-                        <h5 class="font-weight-bold text-uppercase mb-4">Address</h5>
-
-                        <ul class="list-unstyled text-footer-cuerpo">
-                            <li>
-                                <p>
-                                    <i class="mr-3"></i> New York, NY 10012, US</p>
-                            </li>
-                            <li>
-                                <p>
-                                    <i class="mr-3"></i> info@example.com</p>
-                            </li>
-                            <li>
-                                <p>
-                                    <i class="mr-3"></i> + 01 234 567 88</p>
-                            </li>
-                            <li>
-                                <p>
-                                    <i class="mr-3"></i> + 01 234 567 89</p>
-                            </li>
-                        </ul>
-
-                    </div>
-                    <!-- Grid column -->
+                       </div>
+                       <!-- Grid column -->
 
 
-                    <!-- Grid column -->
-                    <div class="col-sm-3">
+                       <!-- Grid column -->
+                       <div class="col-sm-3">
 
-                        <!-- Social buttons -->
-                        <h5 class="font-weight-bold text-uppercase mb-4">Follow Us</h5>
+                           <!-- Links -->
+                           <h5 class="font-weight-bold text-uppercase mb-4">About</h5>
 
-                        <div class="row">
+                           <ul class="list-unstyled text-footer-cuerpo ">
+                               <li>
+                                   <p>
+                                       <a class="footer-letra" href="calidad.php">CALIDAD</a>
+                                   </p>
+                               </li>
+                               <li>
+                                   <p>
+                                       <a class="footer-letra" href="productos.php">PRODUCTOS</a>
+                                   </p>
+                               </li>
+                               <li>
+                                   <p>
+                                       <a class="footer-letra" href="empleo.php">EMPLEO</a>
+                                   </p>
+                               </li>
+                               <li>
+                                   <p>
+                                       <a class="footer-letra" href="">¡AL GRANO!</a>
+                                   </p>
+                               </li>
+                           </ul>
 
-                            <div class="col">
-                                <a href="www.facebook.com"><img src="img/icons8-facebook-rodeado-de-círculo-50.png"><img></a>
-                            </div>
-                            <div class="col">
-                                <a href="www.instagram.com"><img src="img/icons8-instagram-50.png"><img></a>
-                            </div>
-                            <div class="col">
-                                <a href="www.twitter.es"><img src="img/icons8-twitter-50.png"><img></a>
-                            </div>
-                        </div>
+                       </div>
+                       <!-- Grid column -->
 
-                    </div>
-                    <!-- Grid column -->
+                       <!-- Grid column -->
+                       <div class="col-sm-3">
 
-                </div>
-                <!-- Grid row -->
+                           <!-- Contact details -->
+                           <h5 class="font-weight-bold text-uppercase mb-4">Address</h5>
 
-            </div>
-            <!-- Footer Links -->
+                           <ul class="list-unstyled text-footer-cuerpo">
+                               <li>
+                                   <p>
+                                       <i class="mr-3"></i> New York, NY 10012, US</p>
+                               </li>
+                               <li>
+                                   <p>
+                                       <i class="mr-3"></i> info@example.com</p>
+                               </li>
+                               <li>
+                                   <p>
+                                       <i class="mr-3"></i> + 01 234 567 88</p>
+                               </li>
+                               <li>
+                                   <p>
+                                       <i class="mr-3"></i> + 01 234 567 89</p>
+                               </li>
+                           </ul>
 
-            <!-- Copyright -->
-            <div class="footer-copyright text-center py-3 text-footer-cuerpo">Tr3sCaf€s © 2019 Copyright</div>
-            <!-- Copyright -->
+                       </div>
+                       <!-- Grid column -->
 
-        </footer>
-        <!-- Footer -->
+
+                       <!-- Grid column -->
+                       <div class="col-sm-3">
+
+                           <!-- Social buttons -->
+                           <h5 class="font-weight-bold text-uppercase mb-4">Follow Us</h5>
+
+                           <div class="row">
+
+                               <div class="col">
+                                   <a href="www.facebook.com"><img src="img/icons8-facebook-rodeado-de-círculo-50.png"><img></a>
+                               </div>
+                               <div class="col">
+                                   <a href="www.instagram.com"><img src="img/icons8-instagram-50.png"><img></a>
+                               </div>
+                               <div class="col">
+                                   <a href="www.twitter.es"><img src="img/icons8-twitter-50.png"><img></a>
+                               </div>
+                           </div>
+
+                       </div>
+                       <!-- Grid column -->
+
+                   </div>
+                   <!-- Grid row -->
+
+               </div>
+               <!-- Footer Links -->
+
+               <!-- Copyright -->
+               <div class="footer-copyright text-center py-3 text-footer-cuerpo">Tr3sCaf€s © 2019 Copyright</div>
+               <!-- Copyright -->
+
+           </footer>
+           <!-- Footer -->
+        </div>
     </body>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -3,106 +3,107 @@
         <?php include ('includes/enlaces.php') ?>
     </head>
     <body>
-        <!--NavBar Menu-->
-        <!--NavBar Menu-->
-        <div class="row">
-            <div class="col">
-                <nav class="navbar navbar-expand-lg bg-primary">
-                    <a class="navbar-brand" href="index.php"><img class="imgLogo " src="img/415.png"></a>
+        <div class="container-fluid p-0">
+            <!--NavBar Menu-->
+            <!--NavBar Menu-->
+            <div class="row">
+                <div class="col">
+                    <nav class="navbar navbar-expand-lg bg-primary">
+                        <a class="navbar-brand" href="index.php"><img class="imgLogo " src="img/415.png"></a>
 
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbar">
-                        <i><img class="imgUsu" src="img/Menu_icon_2_icon-icons.com_71856.png"></img></i>
-                    </button>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbar">
+                            <i><img class="imgUsu" src="img/Menu_icon_2_icon-icons.com_71856.png"></img></i>
+                        </button>
 
-                    <div class="collapse navbar-collapse" id="collapsingNavbar">
-                        <ul class="nav navbar-nav navbar-letra">
+                        <div class="collapse navbar-collapse" id="collapsingNavbar">
+                            <ul class="nav navbar-nav navbar-letra">
 
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="" id="collapsingNavbar"
-                                   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    CAFÉ
-                                </a>
-                                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                    <li><a class="dropdown-item" href="calidad.php">Calidad</a></li>
-                                    <li><a class="dropdown-item" href="encuentraCafePerfecto.php">Encuentra tu café perfecto</a></li>
-                                    <li><a class="dropdown-item" href="utensilios.php">Utensilios</a></li>
-                                    <li><a class="dropdown-item" href="elaboracion.php">Elaboración</a></li>
-                                </ul>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="" id="collapsingNavbar"
+                                       data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        CAFÉ
+                                    </a>
+                                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                        <li><a class="dropdown-item" href="calidad.php">Calidad</a></li>
+                                        <li><a class="dropdown-item" href="encuentraCafePerfecto.php">Encuentra tu café perfecto</a></li>
+                                        <li><a class="dropdown-item" href="utensilios.php">Utensilios</a></li>
+                                        <li><a class="dropdown-item" href="elaboracion.php">Elaboración</a></li>
+                                    </ul>
 
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="productos.php" id="navbarDropdownMenuLink"
-                                   aria-haspopup="true" aria-expanded="false">
-                                    PRODUCTOS
-                                </a>
-                                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                    <li class="dropdown-submenu"><a class="dropdown-item dropdown-toggle"
-                                                                    >Nuestros cafés</a>
-                                        <ul class="dropdown-menu">
-                                            <li class="dropdown-item"><a class="dropdown-item" href="./loloccino.php">Loloccino</a>
-                                            <li class="dropdown-item"><a class="dropdown-item" href="./juanmocha.php">Juanmocha</a>
-                                            <li class="dropdown-item"><a class="dropdown-item" href="./yorspresso.php">Yorspresso</a>
-                                        </ul>
-                                    </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="productos.php" id="navbarDropdownMenuLink"
+                                       aria-haspopup="true" aria-expanded="false">
+                                        PRODUCTOS
+                                    </a>
+                                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                        <li class="dropdown-submenu"><a class="dropdown-item dropdown-toggle"
+                                                                        >Nuestros cafés</a>
+                                            <ul class="dropdown-menu">
+                                                <li class="dropdown-item"><a class="dropdown-item" href="./loloccino.php">Loloccino</a>
+                                                <li class="dropdown-item"><a class="dropdown-item" href="./juanmocha.php">Juanmocha</a>
+                                                <li class="dropdown-item"><a class="dropdown-item" href="./yorspresso.php">Yorspresso</a>
+                                            </ul>
+                                        </li>
 
-                                    <li class="dropdown-submenu"><a class="dropdown-item dropdown-toggle"
-                                                                    >Repostería</a>
-                                        <ul class="dropdown-menu">
-                                            <li class="dropdown-item"><a class="dropdown-item" href="./tartas.php">Tarta</a>
-                                            <li class="dropdown-item"><a class="dropdown-item" href="./magdalenas.php">Magdalenas</a>
-                                            <li class="dropdown-item"><a class="dropdown-item" href="./galletas.php">Galletas</a>
-                                            <li class="dropdown-item"><a class="dropdown-item" href="./bizcochos.php">Bizcochos</a>
-                                        </ul>
+                                        <li class="dropdown-submenu"><a class="dropdown-item dropdown-toggle"
+                                                                        >Repostería</a>
+                                            <ul class="dropdown-menu">
+                                                <li class="dropdown-item"><a class="dropdown-item" href="./tartas.php">Tarta</a>
+                                                <li class="dropdown-item"><a class="dropdown-item" href="./magdalenas.php">Magdalenas</a>
+                                                <li class="dropdown-item"><a class="dropdown-item" href="./galletas.php">Galletas</a>
+                                                <li class="dropdown-item"><a class="dropdown-item" href="./bizcochos.php">Bizcochos</a>
+                                            </ul>
 
-                                    </li>
+                                        </li>
 
-                                    <li class="dropdown-submenu"><a class="dropdown-item dropdown-toggle"
-                                                                    >Otros</a>
-                                        <ul class="dropdown-menu">
-                                            <li class="dropdown-item"><a class="dropdown-item" href="./chocolates.php">Chocolate</a>
-                                            <li class="dropdown-item"><a class="dropdown-item" href="./infusiones.php">Infusiones</a>
-                                        </ul>
+                                        <li class="dropdown-submenu"><a class="dropdown-item dropdown-toggle"
+                                                                        >Otros</a>
+                                            <ul class="dropdown-menu">
+                                                <li class="dropdown-item"><a class="dropdown-item" href="./chocolates.php">Chocolate</a>
+                                                <li class="dropdown-item"><a class="dropdown-item" href="./infusiones.php">Infusiones</a>
+                                            </ul>
 
-                                    </li>
+                                        </li>
 
-                                    <li class="dropdown-item"><a class="dropdown-item" href="./novedades.php">Novedades</a>
+                                        <li class="dropdown-item"><a class="dropdown-item" href="./novedades.php">Novedades</a>
 
-                                    </li>
-                                </ul>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="./localizanos.php">LOCALÍZANOS</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="./empleo.php">EMPLEO</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="./quienesSomos.php">QUIENES SOMOS</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">¡AL GRANO!</a>
-                            </li>
-                        </ul>
-                        <div class="ml-auto">
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="./localizanos.php">LOCALÍZANOS</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="./empleo.php">EMPLEO</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="./quienesSomos.php">QUIENES SOMOS</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">¡AL GRANO!</a>
+                                </li>
+                            </ul>
+                            <div class="ml-auto">
 
-                            <div class="dropdown">
-                                <button class="btn bg-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                             
-                                    <img class="imgUsu" src="./img/usuario.png"></img>
-                                
-                                </button>
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
-                                    <a class="dropdown-item" href="#">Action</a>
-                                    <a class="dropdown-item" href="#">Another action</a>
-                                    <a class="dropdown-item" href="#">Something else here</a>
+                                <div class="dropdown">
+                                    <button class="btn bg-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+
+                                        <img class="imgUsu" src="./img/usuario.png"></img>
+
+                                    </button>
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
+                                        <a class="dropdown-item" href="#">Action</a>
+                                        <a class="dropdown-item" href="#">Another action</a>
+                                        <a class="dropdown-item" href="#">Something else here</a>
+                                    </div>
                                 </div>
+
                             </div>
-
                         </div>
-                    </div>
 
 
-                </nav>
+                    </nav>
+                </div>
             </div>
-        </div>
     </body>
 </html>
