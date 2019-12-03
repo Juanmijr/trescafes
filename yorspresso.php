a2b7502834f6d4ef54de50691f5dc86ead13b525
yorch4 <jorge.rgdaw@gmail.com>
2019-12-03 13:52:47 +0100

    ETIQUETAS JORGE CAMBIADAS

--- yorspresso.php
+++ yorspresso.php
@@ -19,16 +19,16 @@ and open the template in the editor.
                             <a href="productos.php"> <img src="img/volver.png"></a>
                         </div>
                     </div>
-                    <div class="row mt-5  mx-auto">
+                    <article class="row mt-5  mx-auto">
                         <div class="col-sm-12">
                             <p class="text-title">YORSPRESSO</p>
                             <p class="text-secondary text-justify">Lorem Ipsum is simply dummy text ofry. Lorem Ipsum has been the industry's  centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
                         </div>
-                    </div>
+                    </article>
                 </div>
-                <div class="col-sm-8">
+                <aside class="col-sm-8">
                     <img class="imgProductos img-fluid" src="img/Prueba.png">
-                </div>
+                </aside>
             </div>
         </div>
         <?php
