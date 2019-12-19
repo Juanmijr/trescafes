4270465b5bc0101af7545a833ffa2f61cdc0a545
yorch4 <jorge.rgdaw@gmail.com>
2019-12-19 15:44:26 +0100

    graficos

--- galletas.php
+++ galletas.php
@@ -11,26 +11,56 @@ and open the template in the editor.
     </head>
     <body>
         <?php include ('includes/navbar.php'); ?>
-        <div>
-            <div class="row">
-                <div class="col-sm-4 mt-5">
-                    <div class="row">
-                        <div class="col-sm-12">
-                            <a href="productos.php"> <img src="img/volver.png"></a>
+        <div class="row">
+            <div class="col-sm-4 mt-5">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <a href="productos.php"> <img src="img/volver.png"></a>
+                    </div>
+                </div>
+                <article class="row mt-5  mx-auto">
+                    <div class="col-sm-12">
+                        <p class="text-title">GALLETAS</p>
+                        <p class="text-secondary text-justify">Podrás probar nuestras galletas de limón, de vainilla, con pepitas de chocolate o, si lo prefieres, bajas en azúcar.</p>
+                    </div>
+                </article>
+            </div>
+            <aside class="col-sm-8 mt-5">
+                <img class="imgProductos img-fluid" src="img/Productos/galletas.png">
+            </aside>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <hr>
+                <a class="text-secondary" data-toggle="collapse" href="#footwear" aria-expanded="false" aria-controls="footwear">Info. Nutricional </a>
+                <div class="collapse" id="footwear">
+                    <div class="row collapse" id="footwear">
+                        <div class="chartContainer col-12 my-5">
+                            <canvas id="doughnutChart"></canvas>
                         </div>
                     </div>
-                    <article class="row mt-5  mx-auto">
-                        <div class="col-sm-12">
-                            <p class="text-title">GALLETAS</p>
-                            <p class="text-secondary text-justify">Podrás probar nuestras galletas de limón, de vainilla, con pepitas de chocolate o, si lo prefieres, bajas en azúcar.</p>
-                        </div>
-                    </article>
                 </div>
-                <aside class="col-sm-8 mt-5">
-                    <img class="imgProductos img-fluid" src="img/Productos/galletas.png">
-                </aside>
+                <hr>
             </div>
         </div>
+        <script>//Chart
+            var ctxD = document.getElementById("doughnutChart").getContext('2d');
+            var myLineChart = new Chart(ctxD, {
+                type: 'doughnut',
+                data: {
+                    labels: ["Proteinas(g)", "Carbohidratos(g)", "Grasas(g)"],
+                    datasets: [{
+                            data: [0.27, 3.45, 1.07],
+                            backgroundColor: ["#ba9b6b", "#6f6f6f", "#be8d5f"],
+                            hoverBackgroundColor: ["#bfab8c", "#858585", "#dcaa7b"]
+                        }]
+                },
+                options: {
+                    maintainAspectRatio: false,
+                    responsive: true
+                }
+            });
+        </script>
         <?php
         include('includes/footer.php');
         ?>
--- yorspresso.php
+++ yorspresso.php
@@ -11,26 +11,56 @@ and open the template in the editor.
     </head>
     <body>
         <?php include ('includes/navbar.php'); ?>
-        <div>
-            <div class="row">
-                <div class="col-sm-4 mt-5">
-                    <div class="row">
-                        <div class="col-sm-12">
-                            <a href="productos.php"> <img src="img/volver.png"></a>
+        <div class="row">
+            <div class="col-sm-4 mt-5">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <a href="productos.php"> <img src="img/volver.png"></a>
+                    </div>
+                </div>
+                <article class="row mt-5  mx-auto">
+                    <div class="col-sm-12">
+                        <p class="text-title">YORSPRESSO</p>
+                        <p class="text-secondary text-justify">Preparado con café recién molido y una buena capa espesa de crema, nuestro café Yorspresso dejará una experiencia muy aromática e intensa.</p>
+                    </div>
+                </article>
+            </div>
+            <aside class="col-sm-8 mt-5">
+                <img class="imgProductos img-fluid" src="img/Productos/Espresso.png">
+            </aside>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <hr>
+                <a class="text-secondary" data-toggle="collapse" href="#footwear" aria-expanded="false" aria-controls="footwear">Info. Nutricional </a>
+                <div class="collapse" id="footwear">
+                    <div class="row collapse" id="footwear">
+                        <div class="chartContainer col-12 my-5">
+                            <canvas id="doughnutChart"></canvas>
                         </div>
                     </div>
-                    <article class="row mt-5  mx-auto">
-                        <div class="col-sm-12">
-                            <p class="text-title">YORSPRESSO</p>
-                            <p class="text-secondary text-justify">Preparado con café recién molido y una buena capa espesa de crema, nuestro café Yorspresso dejará una experiencia muy aromática e intensa.</p>
-                        </div>
-                    </article>
                 </div>
-                <aside class="col-sm-8 mt-5">
-                    <img class="imgProductos img-fluid" src="img/Productos/Espresso.png">
-                </aside>
+                <hr>
             </div>
         </div>
+        <script>//Chart
+            var ctxD = document.getElementById("doughnutChart").getContext('2d');
+            var myLineChart = new Chart(ctxD, {
+                type: 'doughnut',
+                data: {
+                    labels: ["Proteinas(g)", "Grasas(g)"],
+                    datasets: [{
+                            data: [0.07, 0.11],
+                            backgroundColor: ["#ba9b6b", "#6f6f6f"],
+                            hoverBackgroundColor: ["#bfab8c", "#858585"]
+                        }]
+                },
+                options: {
+                    maintainAspectRatio: false,
+                    responsive: true
+                }
+            });
+        </script>
         <?php
         include('includes/footer.php');
         ?>
