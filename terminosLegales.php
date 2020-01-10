f9ddd08487711a607d8d72693072f85457f23f87
DWES <DWES@10.0.2.15>
2020-01-10 13:37:50 +0100

    terminos

--- terminosLegales.php
+++ terminosLegales.php
@@ -14,48 +14,30 @@ and open the template in the editor.
 
         <article>
             <section class="row">
-                
+
                 <div class="col-sm text-center pl-5 text-title">
                     TÉRMINOS LEGALES
                 </div>
-               
+
                 </div>
             </section>
-            
+
             <section class="row">
-                <div class="col-sm pl-10 pt-5 text-secondary text-justify">
-                    Las imágenes utilizadas para nuestros productos, así como su respectiva descripción, han sido sacadas de la web gratuita <a href="https://pixabay.com/es/">Pixabay</a>. Esta web nos permite la descarga de imágenes de manera gratuita y nosotros hemos modificado esa imágen para nuestro uso.                    
-                </div>
-                
-            </section>
-            
-            <section class="row">
-                <div class="col-sm pl-10 pt-5 text-secondary text-justify">
-                    Para la página de empleo, se usan tres imágenes pequeñas, que han sido sacadas de la pagina <a href="https://www.flaticon.es/">Flaticon</a>. Esta página es la mayor base de datos de iconos gratuitos disponibles en distintos formatos.<br>
-                    Tambíen se usa una imagen de fondo para el botón de registro en esa misma página. Esta imagen ha sido sacad de Pixabay, anteriormente mencionada.
-                </div>
-             
-            </section>
-            
-            <section class="row">
-                <div class="col-sm pt-5 pl-10 text-secondary text-justify">
-                    Para la página de elaboración se usa un vídeo, el cuál ha sido sacado de youtube. El vídeo es este: <a href="https://www.youtube.com/watch?v=nAqSeHbEI5I">Ver vídeo en YouTube</a>
-                </div>
-               
-            </section>
-            
-            <section class="row">
-                <div class="col-sm pt-5 pl-10 text-secondary text-justify">
+                <div class="col-sm p-5 text-secondary text-justify">
+                    <p>Las imágenes utilizadas para nuestros productos, así como su respectiva descripción, han sido sacadas de la web gratuita <a href="https://pixabay.com/es/">Pixabay</a>. Esta web nos permite la descarga de imágenes de manera gratuita y nosotros hemos modificado esa imágen para nuestro uso.</p>                    
+                    <p>Para la página de empleo, se usan tres imágenes pequeñas, que han sido sacadas de la pagina <a href="https://www.flaticon.es/">Flaticon</a>. Esta página es la mayor base de datos de iconos gratuitos disponibles en distintos formatos.<br>
+                        Tambíen se usa una imagen de fondo para el botón de registro en esa misma página. Esta imagen ha sido sacad de Pixabay, anteriormente mencionada.</p>
+                    <p>Para la página de elaboración se usa un vídeo, el cuál ha sido sacado de youtube. El vídeo es este: <a href="https://www.youtube.com/watch?v=nAqSeHbEI5I">Ver vídeo en YouTube</a></p>
                     <p>
                         En el home, se utilizan imágenes reutilizables, las cuales las he modificado también para no tener problemas con los derechos de autor. 
                         Aún así, son cogidas de :  <br>
                         -<a href="https://www.google.es/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj0tvfdjvfmAhULXRoKHRkBCNgQjB16BAgBEAM&url=https%3A%2F%2Fwww.publicdomainpictures.net%2Fes%2Fview-image.php%3Fimage%3D291713%26picture%3Dcapuchino&psig=AOvVaw2Mwf5LUhZqgAubv_ygmNQq&ust=1578679809417203"> Public domain pictures</a>
                         <br>
-                        
                         Las demás imagenes son imagenes reutilizadas sin derechos de autor, siendo de nuestra empresa, dejándolas libres para que puedan usarlas. 
                     </p>
                 </div>
             </section>
+            
         </article>
 
         <?php
