693f1970756517757cfea1031a650633f9472253
DWES <DWES@10.0.2.15>
2020-01-10 09:24:20 +0100

    a

--- quienesSomos.php
+++ quienesSomos.php
@@ -29,7 +29,7 @@ and open the template in the editor.
         </div>
         <div class="row">
             <aside class="col-sm-4 pt-5">
-                <img src="img/quienesSomos.png">
+                <img class="img-fluid" src="img/quienesSomos.png">
             </aside>
             <section class="col-sm-8 text-justify text-secondary pt-5">
                 <p class="text-title">NUESTROS VALORES</p><br>
