fabe19ea9e6da7704e26fd5916db98751d726745
DWES <DWES@10.0.2.15>
2020-01-10 14:52:24 +0100

    ""

--- elaboracion.php
+++ elaboracion.php
@@ -1,6 +1,7 @@
 <html>
     <head>
         <?php include ('includes/head.php'); ?>
+        <script src="script.js"></script>
         <title>Elaboración | Tres Cafés</title>
     </head>
     <body>
@@ -10,53 +11,69 @@
 
         <div class="container-fluid">
             <main class="row mt-5">
-                 <article class=" col-md-12">
+                <article class=" col-md-12">
                     <h1 class="text-title">CÓMO ELABORAR CAFÉ</h1>
                     <hr>
                     <h3 class="text-justify text-secondary">Elaboración del café en 8 sencillos pasos</h3>
-                    <div class="embed-responsive embed-responsive-16by9" >
-                        <iframe class="embed-responsive-item" src="videos/procesodelcafepasoapaso.mp4" allowfullscreen=""></iframe>
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <div id="video-container">
+                            <video id="video" width="640" height="365">
+                                <source src="videos/procesodelcafepasoapaso.mp4" type="video/webm">
+                                <p>
+                                    Your browser doesn't support HTML5 video.
+                                    <a href="videos/mikethefrog.mp4">Download</a> the video instead.
+                                </p>
+                            </video>
+                            <div id="video-controls">
+                                <button type="button" id="play-pause" class="play">Play</button>
+                                <input type="range" id="seek-bar" value="0">
+                                <button type="button" id="mute">Mute</button>
+                                <input type="range" id="volume-bar" min="0" max="1" step="0.1" value="1">
+                                <button type="button" id="full-screen">Full-Screen</button>
+                            </div>
+                        </div>  
                     </div>
+                    
                     <hr>
                     <div  class="pt-4 pb-5 text-justify text-secondary pl-5 pr-5">
                         <p>
-                            <h4 class="text-title">1.-  PLANTACIÓN</h4>
-                            Un grano de café en realidad es una semilla. Proviene del cafeto y en los cultivos se mantiene a un tamaño de alrededor de 3 metros.
+                        <h4 class="text-title">1.-  PLANTACIÓN</h4>
+                        Un grano de café en realidad es una semilla. Proviene del cafeto y en los cultivos se mantiene a un tamaño de alrededor de 3 metros.
                         </p>
                         <p>
-                            <h4 class="text-title">2.-  COSECHA</h4>
-                            En esta fase se recolectan las "cerezas", que son los frutos del cafeto. Se suele realizar a mano, seleccionando solo los granos que tengan el punto justo de madurez.
+                        <h4 class="text-title">2.-  COSECHA</h4>
+                        En esta fase se recolectan las "cerezas", que son los frutos del cafeto. Se suele realizar a mano, seleccionando solo los granos que tengan el punto justo de madurez.
                         </p>
                         <p>
-                            <h4 class="text-title">3.-  PROCESAMIENTO</h4>
-                            Existen dos métodos, el seco y el húmedo.<br>
-                            En el seco, los frutos se extienden en grandes superficies para su secado al sol.<br>
-                            En el húmedo, se separa la pulpa del fruto y luego el grano se seca con la cáscara pergamino que lo rodea.
+                        <h4 class="text-title">3.-  PROCESAMIENTO</h4>
+                        Existen dos métodos, el seco y el húmedo.<br>
+                        En el seco, los frutos se extienden en grandes superficies para su secado al sol.<br>
+                        En el húmedo, se separa la pulpa del fruto y luego el grano se seca con la cáscara pergamino que lo rodea.
                         </p>
                         <p>
-                            <h4 class="text-title">4.-  CURADO</h4>
-                            Cuando se emplea el método húmedo, se retira el pergamino que rodea al grano. Sin embargo, si se ha procesado en seco, se elimina totalmente la cáscara seca del fruto.
+                        <h4 class="text-title">4.-  CURADO</h4>
+                        Cuando se emplea el método húmedo, se retira el pergamino que rodea al grano. Sin embargo, si se ha procesado en seco, se elimina totalmente la cáscara seca del fruto.
                         </p>
                         <p>
-                            <h4 class="text-title">5.-  DEGUSTACIÓN</h4>
-                            En cada etapa de producción, el café se somete a pruebas de calidad y sabor. En el cupping se analizan diversos factores como el cuerpo, la aroma, la acidez y el sabor.
+                        <h4 class="text-title">5.-  DEGUSTACIÓN</h4>
+                        En cada etapa de producción, el café se somete a pruebas de calidad y sabor. En el cupping se analizan diversos factores como el cuerpo, la aroma, la acidez y el sabor.
                         </p>
                         <p>
-                            <h4 class="text-title">6.-  TUESTE</h4>
-                            Aquí los granos verdes adquieren el tono oscuro que conocemos.  Además, cuando la temperatura alcanza los 200 ºC, se liberan los aceites. Cuanto más aceite hay, más sabor tiene el café.
+                        <h4 class="text-title">6.-  TUESTE</h4>
+                        Aquí los granos verdes adquieren el tono oscuro que conocemos.  Además, cuando la temperatura alcanza los 200 ºC, se liberan los aceites. Cuanto más aceite hay, más sabor tiene el café.
                         </p>
                         <p>
-                            <h4 class="text-title">7.-  MOLIDO</h4>
-                            El café se muele con el fin de obtener más sabor. El grado de espesor de la molienda determinará el proceso de elaboración de la bebida final.
+                        <h4 class="text-title">7.-  MOLIDO</h4>
+                        El café se muele con el fin de obtener más sabor. El grado de espesor de la molienda determinará el proceso de elaboración de la bebida final.
                         </p>
                         <p>
-                            <h4 class="text-title">8.-  PREPARACIÓN</h4>
-                            Listo para el consumo del café. Existen infinidad de métodos para ello. 
+                        <h4 class="text-title">8.-  PREPARACIÓN</h4>
+                        Listo para el consumo del café. Existen infinidad de métodos para ello. 
                         </p>
                     </div>
 
                 </article>
-                
+
             </main>
         </div>
 
